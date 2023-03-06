bf48a97da4e78b1dd339e0eba8b61acf42368dba
seomKim <hs.kim@3billion.io>
2023-03-06 13:02:26 +0900

    error handling println!

--- guessing_game_loop/src/main.rs
+++ guessing_game_loop/src/main.rs
@@ -1,33 +1,38 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 fn main() {
     println!("Guess the number!");
-    
-    
+
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {secret_number}");
 
-    loop{// break this loop when the equal condition met.
+    loop {
+        // break this loop when the equal condition met.
         println!("Please input your guess.");
-        
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-        let guess:u32 = match guess.trim().parse() {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
             Ok(guess) => guess,
-            Err(_) => continue
+            Err(err) => {
+                println!("{err}");
+                continue;
+            }
         };
+
         println!("You guessed: {guess}");
-    
-        match guess.cmp(&secret_number){
+
+        match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => { 
+            Ordering::Equal => {
                 println!("Got it!");
-                break
+                break;
             }
         }
-
-    }    
+    }
 }
