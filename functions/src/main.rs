c978dd997a5d02f7cf6986a5033578f043b476ba
seomKim <hs.kim@3billion.io>
2023-03-09 08:54:01 +0900

    statement

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    // statement는 return 값이 없음.
+    // 그래서 let x = y=6 은 let x= nothing 과 같다.
+    // println! 안에서 {x:?} 는 debug trait 형식으로 출력하기 위함.
+    let y;
+    let x = 1;
+    let x = y = 6;
+    println!("x: {x:?}, y: {y}");
 }
